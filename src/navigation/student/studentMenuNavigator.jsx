f300cd7bed7c216b32626f52a9831568853d1186
Gagnub <www.gaganb@gmail.com>
2025-10-18 15:23:55 +0530

    studentProfileScreen import fix in studentMenuNavigator.

--- src/navigation/student/studentMenuNavigator.jsx
+++ src/navigation/student/studentMenuNavigator.jsx
@@ -7,7 +7,7 @@ import NoticeBoardScreen from '../../screens/StudentParent/menuscreen/NoticeBoar
 import StudentPerformanceScreen from '../../screens/StudentParent/menuscreen/StudentPerformanceScreen';
 import ParentProfileScreen from '../../screens/StudentParent/menuscreen/ParentProfileScreen';
 import AcademicCalendarScreen from '../../screens/StudentParent/menuscreen/AcademicCalendarScreen';
-import StudentProfileScreen from '../../screens/StudentParent/homescreen/studentProfileScreen';
+import StudentProfileScreen from '../../screens/StudentParent/profilescreen/studentProfileScreen';
 import FeesScreen from '../../screens/StudentParent/menuscreen/settings/FeesScreen';
 import ChangePasswordScreen from '../../screens/StudentParent/menuscreen/settings/ChangePasswordScreen';
 import PrivacyPolicyScreen from '../../screens/StudentParent/menuscreen/settings/PrivacyPolicyScreen';
